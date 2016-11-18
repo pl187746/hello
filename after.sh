9eae156840c66cdc4750cd9d394b85a4719791f6
Karol <187746@edu.p.lodz.pl>
2016-11-18 23:35:26 +0100

    lol

--- after.sh
+++ after.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 rm -rf depbin
-git clone --branch=bin --depth=5 https://github.com/pl187746/hello depbin
+git clone --branch=bin --depth=5 https://pl187746:$ASDF@github.com/pl187746/hello depbin
 cd depbin
-rm -f *.jar
-cp -f ../target/*.jar ./
+rm -rf target
+mkdir target
+cp -f ../target/*.jar ./target/
 git config --local user.name Travis
 git config --local user.email 187746@edu.p.lodz.pl
 git add -A
 git commit -m "build"
-git push -f https://pl187746:$ASDF@github.com/pl187746/hello bin
+git push -f
 cd ..
 rm -rf depbin
