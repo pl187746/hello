aa04a499371ea54242f8e7b3599d9c70031096b3
Karol <187746@edu.p.lodz.pl>
2016-11-18 23:18:36 +0100

    git

--- after.sh
+++ after.sh
@@ -4,6 +4,8 @@ git clone --branch=bin --depth=5 https://github.com/pl187746/hello depbin
 cd depbin
 rm -f *.jar
 cp -f ../target/*.jar ./
+git config --local user.name Travis
+git config --local user.email 187746@edu.p.lodz.pl
 git add -A
 git commit -m "build"
 cd ..
