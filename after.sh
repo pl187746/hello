683df3e92e245387a7bbb72f9f12ee10163d2035
Karol <187746@edu.p.lodz.pl>
2016-11-18 23:13:48 +0100

    afdfndgf

--- after.sh
+++ after.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 rm -rf depbin
+git clone --branch=bin --depth=5 https://github.com/pl187746/hello depbin
 cd depbin
+rm -f *.jar
 cp -f ../target/*.jar ./
+git add -A
+git commit -m "build"
 cd ..
 rm -rf depbin
